3e593bbaa684fcd94a30476b33b8211084af39bd
Joshua <joshua537.nit@gmail.com>
2024-05-06 16:52:18 +0530

    Update list_accessible_files.sh

--- week 3/list_accessible_files.sh
+++ week 3/list_accessible_files.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Get a list of files in the current directory
+
+# Loop through each file and check permissions
+# for file in <list_of_files>; do
+#     if [ <condition_to_check_read_permission> ] && [ <condition_to_check_write_permission> ] && [ <condition_to_check_execute_permission> ]; then
+#         echo "$file"
+#     fi
+# done
 
