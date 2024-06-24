599e0da0a8409cd169c4f2103e4f68ce733d3ee1
Joshua <joshua537.nit@gmail.com>
2024-06-24 14:53:29 +0530

    Create process4.c

---
+++ week 11/process4.c
@@ -0,0 +1,40 @@
+// process4.c
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+struct message {
+    long msg_type;
+    char msg_text[MAX];
+};
+
+int main() {
+    key_t key;
+    int msgid;
+    struct message msg;
+
+    // Generate unique key
+    key = ftok("progfile", 65);
+
+    // Get the message queue id
+    msgid = msgget(key, 0666 | IPC_CREAT);
+    if (msgid == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    // Send request
+    msg.msg_type = 1;
+    sprintf(msg.msg_text, "iseven 24");
+    msgsnd(msgid, &msg, sizeof(msg), 0);
+
+    // Receive response
+    msgrcv(msgid, &msg, sizeof(msg), 2, 0);
+    printf("Response: %s\n", msg.msg_text);
+
+    return 0;
+}
