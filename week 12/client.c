9078c138bf3b8230d14464d85489b57d0c23658a
Joshua <joshua537.nit@gmail.com>
2024-06-24 15:02:08 +0530

    Create client.c

---
+++ week 12/client.c
@@ -0,0 +1,44 @@
+// client_isprime.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define MAX 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[MAX] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    char *message = "isprime 17";
+    send(sock, message, strlen(message), 0);
+    printf("Request sent: %s\n", message);
+
+    int valread = read(sock, buffer, MAX);
+    printf("Response: %s\n", buffer);
+
+    close(sock);
+    return 0;
+}
